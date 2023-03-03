18665dae9177b2d430ee7769cbb332893055f01c
Zoe Sente <zsente@mi-r2-p4.s19.be>
2023-03-03 18:26:35 +0100

    test non strict ok

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: zoesente <zoesente@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/30 15:05:34 by zoesente          #+#    #+#             */
-/*   Updated: 2023/02/01 00:34:33 by zoesente         ###   ########.fr       */
+/*   Updated: 2023/03/03 18:18:04 by zsente           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,7 +36,7 @@ void	ft_putstr_fd(char *s, int fd)
 int	ft_printchar(char c)
 {
 	write(1, &c, 1);
-    return(1); 
+	return (1);
 }
 
 size_t	ft_strlen(const char *s)
@@ -52,9 +52,9 @@ size_t	ft_strlen(const char *s)
 int	ft_printstr(char *str)
 {
 	int	i;
-	
-	i = 0; 
-	if(str)
+
+	i = 0;
+	if (str)
 	{
 		while (str[i])
 		{
@@ -62,150 +62,162 @@ int	ft_printstr(char *str)
 			i++;
 		}
 	}
-	return(i); 
+	else
+	{
+		ft_putstr_fd("(null)", 1);
+		return (6);
+	}
+	return (i);
 }
 
-int ft_numberlength(int nb)
+int	ft_numberlength(int nb)
 {
-	int i; 
-    i = 1; 
-    if(nb < 0)
-    {
-        nb *=-1; 
-        i++; 
-    }
+	int	i;
+
+	i = 1;
+	if (nb == -2147483648)
+	{
+		return (11);
+	}
+	else if (nb < 0)
+	{
+		nb *= -1;
+		i++;
+	}
 	while (nb >= 10)
 	{
-		nb = nb / 10; 
-		i++; 
+		nb = nb / 10;
+		i++;
 	}
-	return(i); 
+	return (i);
 }
 
-int ft_hexlength(unsigned long nb, unsigned long int length_base)
+int	ft_hexlength(unsigned long nb, unsigned long int length_base)
 {
-    unsigned int i; 
+	unsigned int	i;
 
-    i = 1; 
+	i = 1;
 	while (nb >= length_base)
 	{
-		nb = nb / length_base; 
-		i++; 
+		nb = nb / length_base;
+		i++;
 	}
-	return(i); 
-
+	return (i);
 }
-void ft_putnbr_base(int nb, int length_base, char* base)
+
+void	ft_putnbr_base(int nb, int length_base, char *base)
 {
-	if (n == -2147483648)
+	if (nb == -2147483648)
 	{
 		ft_putstr_fd("-2147483648", 1);
 	}
 	else if (nb < 0)
-	{ 
+	{
 		nb *= -1;
-		ft_putchar_fd('-', 1); 
-		ft_putnbr_base(nb, length_base, base); 
+		ft_putchar_fd('-', 1);
+		ft_putnbr_base(nb, length_base, base);
 	}
 	else if (nb >= length_base)
 	{
 		ft_putnbr_base(nb / length_base, length_base, base);
-		ft_putnbr_base(nb % length_base, length_base, base); 
+		ft_putnbr_base(nb % length_base, length_base, base);
 	}
 	else
-    {
+	{
 		ft_putchar_fd(base[nb], 1);
-    }
+	}
 }
 
-void ft_puthex_base(unsigned long int nb, unsigned int length_base, char* base)
+void	ft_puthex_base(unsigned long int nb, unsigned int length_base, char *base)
 {
 	if (nb >= length_base)
 	{
 		ft_puthex_base(nb / length_base, length_base, base);
-		ft_puthex_base(nb % length_base, length_base, base); 
+		ft_puthex_base(nb % length_base, length_base, base);
 	}
 	else
-    {
+	{
 		ft_putchar_fd(base[nb], 1);
-    }
+	}
 }
 
-int ft_printnbr(int nb)
+int	ft_printnbr(int nb)
 {
-    int len; 
-    
-    len = ft_numberlength(nb); 
-    ft_putnbr_base(nb, 10, "0123456789"); 
-    return(len); 
+	int	len;
+
+	len = ft_numberlength(nb);
+	ft_putnbr_base(nb, 10, "0123456789");
+	return (len);
 }
 
 int	ft_printhex_unsigned(unsigned int nb, const char format)
 {
-    char *base; 
-    int len; 
-    int length_base; 
-    if(format == 'u')
-        base = "0123456789"; 
-	if(format == 'x')
-        base = "0123456789abcdef";
-    if(format == 'X')
-        base = "0123456789ABCDEF";
-     length_base = ft_strlen(base); 
-    len = ft_hexlength(nb, length_base); 
-    ft_puthex_base(nb, length_base, base); 
-    return(len); 
+	char	*base;
+	int		len;
+	int		length_base;
+
+	if (format == 'u')
+		base = "0123456789";
+	if (format == 'x')
+		base = "0123456789abcdef";
+	if (format == 'X')
+		base = "0123456789ABCDEF";
+	length_base = ft_strlen(base);
+	len = ft_hexlength(nb, length_base);
+	ft_puthex_base(nb, length_base, base);
+	return (len);
 }
 
-int ft_printptr(unsigned long int ptr)
+int	ft_printptr(unsigned long int ptr)
 {
-    int len; 
-    len = ft_hexlength(ptr, 16); 
-    ft_putstr_fd("0x", 1); 
-    ft_puthex_base(ptr, 16, "0123456789abcdef"); 
-    return(len + 2);
+	int	len;
+
+	len = ft_hexlength(ptr, 16);
+	ft_putstr_fd("0x", 1);
+	ft_puthex_base(ptr, 16, "0123456789abcdef");
+	return (len + 2);
 }
 
-int ft_print_args(const char *format, va_list args)
+int	ft_print_args(const char *format, va_list args)
 {
-	int i; 
-	int len; 
+	int	i;
+	int	len;
 
-	i = 0; 
-	len = 0; 
-	while(format[i])
+	i = 0;
+	len = 0;
+	while (format[i])
 	{
-		if(format[i] =='%')
+		if (format[i] == '%')
 		{
-			if(format[i + 1] == 'c')
-				len += ft_printchar(va_arg(args, int)); 
-            if(format[i + 1] == 's')
-                len += ft_printstr(va_arg(args, char*));
-            if(format[i + 1] == 'p')
-                len += ft_printptr(va_arg(args, unsigned long int)); 
-            if(format[i + 1] == 'd' || format[i + 1] == 'i')
-                len += ft_printnbr(va_arg(args, int));
-            if(format[i + 1] == 'x' || format[i + 1] == 'X' || format[i + 1] == 'u')
-                len += ft_printhex_unsigned(va_arg(args, int), format[i + 1]); 
-            if(format[i + 1] == '%')
-                len += ft_printchar('%'); 
-			i++; 
+			if (format[i + 1] == 'c')
+				len += ft_printchar(va_arg(args, int));
+			if (format[i + 1] == 's')
+				len += ft_printstr(va_arg(args, char *));
+			if (format[i + 1] == 'p')
+				len += ft_printptr(va_arg(args, unsigned long int));
+			if (format[i + 1] == 'd' || format[i + 1] == 'i')
+				len += ft_printnbr(va_arg(args, int));
+			if (format[i + 1] == 'x' || format[i + 1] == 'X' || format[i + 1] == 'u')
+				len += ft_printhex_unsigned(va_arg(args, int), format[i + 1]);
+			if (format[i + 1] == '%')
+				len += ft_printchar('%');
+			i++;
 		}
-		else 
-			len += ft_printchar(format[i]); 
+		else
+			len += ft_printchar(format[i]);
 		i++;
 	}
-	return(len); 
+	return (len);
 }
 
-int ft_printf(const char *str, ...)
+int	ft_printf(const char *str, ...)
 {
-	va_list args; 
-	int print_length; 
+	va_list	args;
+	int		print_length;
 
-	print_length = 0; 
-	va_start(args, str); 
+	print_length = 0;
+	va_start(args, str);
 	print_length += ft_print_args(str, args);
-	va_end(args); 
-	return(print_length); 
+	va_end(args);
+	return (print_length);
 }
--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: zoesente <zoesente@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/20 15:03:24 by zoesente          #+#    #+#             */
-/*   Updated: 2023/01/31 23:21:34 by zoesente         ###   ########.fr       */
+/*   Updated: 2023/03/03 18:21:39 by zsente           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,11 @@
 # include <stdarg.h>
 # include <stdlib.h>
 
-int	ft_printchar(char c); 
-int ft_numberlength(int nb);
-int ft_printnbr(int nb); 
-int	ft_printstr(char *str);
-int ft_printformat( const char format, va_list args); 
-int ft_printf(const char *str, ...); 
-void ft_putnbr_base(int nb, int length_base, char* base); 
-
+int		ft_printchar(char c);
+int		ft_numberlength(int nb);
+int		ft_printnbr(int nb);
+int		ft_printstr(char *str);
+int		ft_printformat( const char format, va_list args);
+int		ft_printf(const char *str, ...);
+void	ft_putnbr_base(int nb, int length_base, char *base);
 #endif
-
